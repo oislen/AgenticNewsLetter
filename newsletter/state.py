4042087a42ea0db1fed2d61da9865599c7a1fc87
Oisin <oisin.leonard@gmail.com>
2026-05-10 17:10:25 +0100

    Added subtopic

--- newsletter/state.py
+++ newsletter/state.py
@@ -2,6 +2,7 @@ from typing import TypedDict, List
 
 class NewsletterState(TypedDict):
     topic: str
+    subtopic: str
     style: str  # Options: "academic", "ELI5", "tutorial"
     research_data: str
     newsletter_draft: str
