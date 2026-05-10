202476d739f99f4755feb61c011ba98d06378858
Oisin <oisin.leonard@gmail.com>
2026-05-10 17:05:51 +0100

    Added file paths to cons.py

--- newsletter/cons.py
+++ newsletter/cons.py
@@ -1,5 +1,16 @@
 import re
 import os
+import sys
+
+# set file directories
+base_dir = os.getcwd().split("AgenticNewsLetter")[0]
+root_dir = os.path.join(base_dir, "AgenticNewsLetter")
+newletter_dir = os.path.join(root_dir, "newletter")
+creds_dir = os.path.join(root_dir, ".creds")
+tavily_api_fpath = os.path.join(creds_dir, "tavily")
+# append file locations to path
+for path in [base_dir, root_dir, newletter_dir, creds_dir]:
+    sys.path.append(path)
 
 # set root file directories
 root_dir_re_match = re.findall(string=os.getcwd(), pattern="^.+AgenticNewsLetter")
