dcc8dd97707a734c990f1ed1799ab9caca17f03f
Oisin <oisin.leonard@gmail.com>
2026-05-11 08:58:51 +0100

    Added get_test_secrets

--- newsletter/utils/__init__.py
+++ newsletter/utils/__init__.py
@@ -2,4 +2,4 @@ from .bedrock_client import bedrock_client
 from .style_guides import style_guides
 from .topics import topics
 from .random_inputs import random_inputs
-from .get_secrets import get_secrets
+from .get_secrets import get_secrets, get_test_secrets
