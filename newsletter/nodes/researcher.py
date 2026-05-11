9f4d06e579f0a6851efd2939abe58ff683966756
Oisin <oisin.leonard@gmail.com>
2026-05-11 09:06:27 +0100

    Removed newline

--- newsletter/nodes/researcher.py
+++ newsletter/nodes/researcher.py
@@ -6,6 +6,5 @@ def researcher_node(state: NewsletterState):
     search = TavilySearch(max_results=5, search_depth="advanced", topic="news")
     query = f"latest breakthroughs and news in {state['topic']} for April 2026"
     results = search.invoke({"query": query})
-    
     context = "\n".join([f"Title: {r['title']}\nURL: {r['url']}\nContent: {r['content']}\n---" for r in results])
     return {"research_data": context, "steps_taken": ["researcher_complete"]}
