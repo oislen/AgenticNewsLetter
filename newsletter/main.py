1986cc80d1209505edaca1b0c3818a2f71709a3b
Oisin <oisin.leonard@gmail.com>
2026-05-11 08:58:09 +0100

    Added option to load test environment

--- newsletter/main.py
+++ newsletter/main.py
@@ -4,24 +4,26 @@ import sys
 
 import cons
 from graph import builder
-from utils import random_inputs, get_secrets
+from utils import random_inputs, get_secrets, get_test_secrets
 
 if __name__ == "__main__":
     graph = builder.compile()
 
     # generate random inputs
     inputs = random_inputs()
-
-    if os.path.exists(cons.tavily_api_fpath):
-        with open(cons.tavily_api_fpath, "r") as file:
-            secrets = {"TAVILY_API_KEY":file.readline()}
+    if cons.localTestEnv:
+        secrets = get_test_secrets(cons)
     else:
         secrets = get_secrets()
+    # assign secrets to runtime environment
     os.environ["TAVILY_API_KEY"] = secrets['TAVILY_API_KEY']
-    
+    os.environ["SENDER_EMAIL"] = secrets['SENDER_EMAIL']
+    os.environ["SENDER_PASSWORD"] = secrets['SENDER_PASSWORD']
+    breakpoint()
+
     # Change 'style' here to switch the newsletter's behavior
     graph.invoke({
-        "topic": inputs['topic'], 
+        "topic": inputs['topic'],
         "subtopic": inputs['subtopic'],
         "style": inputs['style'],
         "steps_taken": []
