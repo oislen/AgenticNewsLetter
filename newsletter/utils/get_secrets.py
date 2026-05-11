b0686a91c7c2b3ef3c9c18d9d98d0cb2a69ff77a
Oisin <oisin.leonard@gmail.com>
2026-05-11 08:58:35 +0100

    Added function to pull test environment secrets

--- newsletter/utils/get_secrets.py
+++ newsletter/utils/get_secrets.py
@@ -25,3 +25,45 @@ def get_secrets():
     client = boto3.client("secretsmanager")
     response = client.get_secret_value(SecretId=secret_arn)
     return json.loads(response['SecretString'])
+
+def get_test_secrets(cons):
+    """
+    Retrieves secrets from local test environment.
+
+    Parameters:
+    -----------
+    cons :
+        The constants python module
+
+    Returns:
+    --------
+    dict
+        A dictionary containing the retrieved test secrets.
+    
+    Example:
+    --------
+    ```
+    secrets = get_secrets()
+    """
+    # define get test secret function
+    def get_test_secret(fpath):
+        with open(fpath, "r") as file:
+            secret = file.readline()
+        return secret
+    # set missing defaults
+    tavily_secret = None
+    email_username = None
+    email_password = None
+    # retrieve test secrets
+    if os.path.exists(cons.tavily_api_fpath):
+        tavily_secret = get_test_secret(cons.tavily_api_fpath)
+    if os.path.exists(cons.email_username_fpath):
+        email_username = get_test_secret(cons.email_username_fpath)
+    if os.path.exists(cons.email_password_fpath):
+        email_password = get_test_secret(cons.email_password_fpath)
+    secrets = {
+        "TAVILY_API_KEY":tavily_secret,
+        "SENDER_EMAIL":email_username,
+        "SENDER_PASSWORD":email_password,
+        }
+    return secrets
