d54e84b87cefcb885685492d25f825e08baa761f
Oisin <oisin.leonard@gmail.com>
2026-05-15 11:06:53 +0100

    Added sender and reciever email details

--- newsletter/cons.py
+++ newsletter/cons.py
@@ -9,8 +9,9 @@ newletter_dir = os.path.join(root_dir, "newletter")
 # creds
 creds_dir = os.path.join(root_dir, ".creds")
 tavily_api_fpath = os.path.join(creds_dir, "tavily")
-email_username_fpath = os.path.join(creds_dir, "email_username")
-email_password_fpath = os.path.join(creds_dir, "email_pass")
+sender_email_username_fpath = os.path.join(creds_dir, "sender_email_username")
+sender_email_password_fpath = os.path.join(creds_dir, "sender_email_pass")
+receiver_email_username_fpath = os.path.join(creds_dir, "receiver_email_username")
 # append file locations to path
 for path in [base_dir, root_dir, newletter_dir, creds_dir]:
     sys.path.append(path)
