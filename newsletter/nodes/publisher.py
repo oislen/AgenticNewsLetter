d578d84c4a158cd525e0dbcce823d16c99018f8e
Oisin <oisin.leonard@gmail.com>
2026-05-15 11:08:31 +0100

    Added receiver email

--- newsletter/nodes/publisher.py
+++ newsletter/nodes/publisher.py
@@ -10,11 +10,12 @@ def publisher_node(state: NewsletterState):
     # Fetching environment variables set in GitHub Secrets
     sender = os.getenv("SENDER_EMAIL")
     pw = os.getenv("SENDER_PASSWORD")
+    receiver = os.getenv("RECEIVER_EMAIL")
     html = markdown.markdown(state['newsletter_draft'])
     msg = MIMEMultipart()
     msg['Subject'] = f"📊 DS Pulse: {state['topic']} - {state['subtopic']}"
     msg['From'] = sender
-    msg['To'] = sender # Sending to yourself
+    msg['To'] = receiver
     msg.attach(MIMEText(f"<html><body>{html}</body></html>", 'html'))
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()
