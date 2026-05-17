af523eba1fd029e3ef778948bd51535bb45576fe
Oisin <oisin.leonard@gmail.com>
2026-05-17 19:01:30 +0100

    Formatted script and added api wrapper to tavily search

--- newsletter/nodes/researcher.py
+++ newsletter/nodes/researcher.py
@@ -1,11 +1,24 @@
 from langchain_tavily import TavilySearch
+from langchain_tavily._utilities import TavilySearchAPIWrapper
 from langchain_core.runnables import RunnableConfig
 
 from state import NewsletterState
 
 def researcher_node(state: NewsletterState, config: RunnableConfig):
-    search = TavilySearch(max_results=5, search_depth="advanced", topic="news")
-    query = f"latest breakthroughs and news in {state['topic']} for April 2026"
+    """
+    """
+    # extract configurable parameters
+    configurable = config.get("configurable", {})
+    # initiate tavily search
+    search = TavilySearch(
+        max_results=5,
+        search_depth="advanced",
+        topic="news",
+        api_wrapper=TavilySearchAPIWrapper(tavily_api_key=configurable.get("TAVILY_API_KEY"))
+    )
+    # define search query
+    query = f"latest breakthroughs and news in {state['topic']} - {state['subtopic']}"
+    # perform search and format results
     search_results = search.invoke({"query": query})
     context = "\n".join([f"Title: {result['title']}\nURL: {result['url']}\nContent: {result['content']}\n---" for result in search_results['results']])
     return {"research_data": context, "steps_taken": ["researcher_complete"]}
